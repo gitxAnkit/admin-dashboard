4733f4a1f2579c100456c88498ae141fac5efc2f
Ankit Verma <ankitv9936@gmail.com>
2023-10-01 16:15:00 +0530

    Create files and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import './styles/global.scss'
+import Home from './pages/home/Home'
+import Products from './pages/products/Products'
+import Users from './pages/users/Users'
+import {
+  createBrowserRouter, RouterProvider, Outlet
+} from "react-router-dom";
+import Footer from './Components/footer/Footer';
+import Menu from './Components/menu/Menu';
+import Navbar from './Components/navbar/Navbar';
+import Login from './pages/login/Login';
 
-const App = () => {
+const Layout =()=>{
   return (
-    <div>App</div>
+    <div className="main">
+      <Navbar/>
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
-export default App
+const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout/>,
+      children:[
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path:"/login",
+      element:<Login />
+    }
+
+  ]); 
+
+
+  return  <RouterProvider router={router}/> ;
+}
+
+export default App;
