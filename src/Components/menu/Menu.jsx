f64573b89b19744a3469a7a26a9be8805e3f5991
Ankit Verma <ankitv9936@gmail.com>
2023-10-01 19:34:46 +0530

    Design Menu

--- src/Components/menu/Menu.jsx
+++ src/Components/menu/Menu.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import "./Menu.scss"
+import { Link } from 'react-router-dom'
+import {menu} from '../../data'
+
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+
+      {menu.map((item)=>(
+
+        <div className="item" key={item.id} >
+
+          <span className="title"> {item.title.toUpperCase()} </span>
+
+          {item.listItems.map((listItem)=>(
+
+            <Link to={listItem.url} className='listItem' key={listItem.id} >
+
+              <img src={listItem.icon} alt="" />
+              <span className="listItemTitle">{listItem.title}</span>
+              
+            </Link>
+
+          ))}
+         
+        </div>
+ 
+      ))}
+
+  
+    </div>
   )
 }
 
