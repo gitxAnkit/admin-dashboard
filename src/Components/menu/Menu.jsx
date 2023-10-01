b06b6b1b002faa090511a6e97ae6cde1c5ea230d
Ankit Verma <ankitv9936@gmail.com>
2023-10-01 20:38:54 +0530

    Make home grid

--- src/Components/menu/Menu.jsx
+++ src/Components/menu/Menu.jsx
@@ -16,7 +16,7 @@ const Menu = () => {
 
           {item.listItems.map((listItem)=>(
 
-            <Link to={listItem.url} className='listItem' key={listItem.id} >
+            <Link to="/" className='listItem' key={listItem.id} >
 
               <img src={listItem.icon} alt="" />
               <span className="listItemTitle">{listItem.title}</span>
