fe23a33f10112b57950562fa0ebc8e3841fd4c18
Ankit Verma <ankitv9936@gmail.com>
2023-10-15 18:17:04 +0530

    Add revenue graph chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,22 +4,24 @@ import TopBox from '../../Components/topBox/TopBox';
 import ChartBox from '../../Components/chartBox/ChartBox';
 import BarChartBox from '../../Components/barChartBox/BarChartBox';
 import PieChartBox from '../../Components/pieChart/PieChartBox';
+import BigChartBox from '../../Components/bigChart/BigChartBox';
 import { chartBoxConversion,
         chartBoxProduct, 
         chartBoxRevenue,
         chartBoxUser,
         barChartBoxVisit, 
         barChartBoxRevenue} from '../../data';
+
 const Home = () => {
   return (
     <div className='home'>
       <div className="box box1"> <TopBox /> </div>
-      <div className="box box2"><ChartBox{... chartBoxUser}/></div>
+      <div className="box box2"><ChartBox {... chartBoxUser}/></div>
       <div className="box box3"><ChartBox {...chartBoxProduct}/> </div>
       <div className="box box4"><PieChartBox /></div>
       <div className="box box5"> <ChartBox {...chartBoxRevenue}/></div>
       <div className="box box6"> <ChartBox {...chartBoxConversion}/></div>
-      <div className="box box7">box7</div>
+      <div className="box box7"><BigChartBox /></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue} /> </div>
     </div>
